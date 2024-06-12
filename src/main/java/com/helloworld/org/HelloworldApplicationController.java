369aa91155fd41a5b7ea8e7c818e9e4892582abe
yuhi1314 <123218375+yuhi1314@users.noreply.github.com>
2024-06-13 06:39:54 +1100

    Update HelloworldApplicationController.java

--- src/main/java/com/helloworld/org/HelloworldApplicationController.java
+++ src/main/java/com/helloworld/org/HelloworldApplicationController.java
@@ -1,3 +1,11 @@
+package com.helloworld.org;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
