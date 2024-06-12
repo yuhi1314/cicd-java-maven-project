786bf0b43c32f062dca8c42d03a4b5e0d6d78aec
yuhi1314 <123218375+yuhi1314@users.noreply.github.com>
2024-06-13 06:28:49 +1100

    Update HelloworldApplication.java

--- src/main/java/com/helloworld/org/HelloworldApplication.java
+++ src/main/java/com/helloworld/org/HelloworldApplication.java
@@ -1,14 +1,17 @@
 package com.helloworld.org;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class HelloController {
-    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
-    @GetMapping("/")
-    public String hello() {
-        logger.info("Received request to / endpoint");
-        return "Hello from the awesome project";
-    }
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplication {
+	private static final Logger logger = LoggerFactory.getLogger(HelloworldApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Server started");
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
 }
