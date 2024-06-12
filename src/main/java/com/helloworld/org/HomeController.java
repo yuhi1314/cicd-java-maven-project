81ec67797b7b78482b831e98bf7e61fb647b18ab
yuhi1314 <123218375+yuhi1314@users.noreply.github.com>
2024-06-13 06:57:25 +1100

    Update HomeController.java

--- src/main/java/com/helloworld/org/HomeController.java
+++ src/main/java/com/helloworld/org/HomeController.java
@@ -1,24 +1,20 @@
 package com.helloworld.org;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
-
   private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
   @GetMapping(
-            path = "/",)
-    public Object status() {
-        logger.info("Status check");
-        return "Service is alive";
-    }
+            path = "/"
+    )
+  @ResponseBody
+  public String status() {
+      logger.info("Status check");
+      return "Service is alive";
+  }
 }
-
