876fdb1c957e49f5874f56eccf045facdd757731
yuhi1314 <123218375+yuhi1314@users.noreply.github.com>
2024-06-13 05:12:11 +1100

    Update HelloworldApplication.java

--- src/main/java/com/helloworld/org/HelloworldApplication.java
+++ src/main/java/com/helloworld/org/HelloworldApplication.java
@@ -1,3 +1,5 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 package com.helloworld.org;
 
 import org.springframework.boot.SpringApplication;
@@ -5,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloworldApplication {
+	private static final Logger logger = LoggerFactory.getLogger(HelloworldApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
