fda773a4e796fc7500a97e52a16279de75a7e517
yuhi1314 <123218375+yuhi1314@users.noreply.github.com>
2024-06-13 05:22:57 +1100

    Update HelloworldApplication.java

--- src/main/java/com/helloworld/org/HelloworldApplication.java
+++ src/main/java/com/helloworld/org/HelloworldApplication.java
@@ -1,6 +1,7 @@
+package com.helloworld.org;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-package com.helloworld.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
