3f46fc074cfdc3da045c6a7083d7237a8573c961
yuhi1314 <123218375+yuhi1314@users.noreply.github.com>
2024-06-14 01:01:32 +1100

    Update HelloworldApplication.java

--- src/main/java/com/helloworld/org/HelloworldApplication.java
+++ src/main/java/com/helloworld/org/HelloworldApplication.java
@@ -1,7 +1,7 @@
 package com.helloworld.org;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
