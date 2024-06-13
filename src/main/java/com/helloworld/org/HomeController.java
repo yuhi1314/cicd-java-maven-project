63e4988b524723b9db8f9ab15ff04b1dcf33ee57
yuhi1314 <123218375+yuhi1314@users.noreply.github.com>
2024-06-14 01:27:33 +1100

    Update HomeController.java

--- src/main/java/com/helloworld/org/HomeController.java
+++ src/main/java/com/helloworld/org/HomeController.java
@@ -2,6 +2,8 @@ package com.helloworld.org;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 import org.springframework.http.ResponseEntity;
